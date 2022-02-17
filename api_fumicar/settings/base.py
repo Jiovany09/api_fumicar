f9980cc025babf023cd018271e7fa07b067d7d76
Jiovany Maruri <20183l301005@utcv.edu.mx>
2022-02-17 15:02:45 -0500

    Creación del archivo keys.py y modificación del archivo base.py

--- api_fumicar/settings/base.py
+++ api_fumicar/settings/base.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from keys import DJANGO_SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-77^9m_lh8s2$k@5*7=0*y*u+6h@w*&&t5+u45@ilo$-z98zf%l'
+SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
